9ad7174785cde1e2722a1b273d0fc544037ee438
Halid Mohamed <150917290+halid435446576@users.noreply.github.com>
2024-07-09 18:36:48 +0300

    Add files via upload
    Python Snake Game Using turtle Module on pycharm

---
+++ main.py
@@ -0,0 +1,42 @@
+from turtle import Screen
+from food import Food
+from snake import Snake
+from score_board import Score
+import time
+
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.bgcolor("black")
+screen.title("Snake game")
+screen.tracer(0)
+
+scoreboard = Score()
+snake = Snake()
+food = Food()
+
+screen.listen()
+screen.onkey(snake.up, "Up")
+screen.onkey(snake.down, "Down")
+screen.onkey(snake.left, "Left")
+screen.onkey(snake.right, "Right")
+
+is_on = True
+while is_on:
+    screen.update()
+    time.sleep(0.1)
+    snake.move()
+
+    if snake.head.distance(food) < 15:
+        food.refresh()
+        snake.extend()
+        scoreboard.increase_score()
+
+    if snake.head.xcor() > 280 or snake.head.ycor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() < -300:
+        scoreboard.game_over()
+        is_on = False
+    for segment in snake.segments[1:]:
+        if snake.head.distance(segment) < 10:
+            scoreboard.game_over()
+            is_on = False
+
+screen.exitonclick()
